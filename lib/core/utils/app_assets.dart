aa81af25e46890d05beb165a60ae72a96c10159a
LamineDem <demlamine.29@gmail.com>
2026-01-06 05:34:43 +0100

    appColors and appAssets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesOnBoarding1
+  /// assets/images/onBoarding1.jpg
+  static const String assetsImagesOnBoarding1 = "assets/images/onBoarding1.jpg";
+
+  /// Assets for assetsImagesOnBoarding2
+  /// assets/images/onBoarding2.jpg
+  static const String assetsImagesOnBoarding2 = "assets/images/onBoarding2.jpg";
+
+  /// Assets for assetsImagesOnBoarding3
+  /// assets/images/onBoarding3.jpg
+  static const String assetsImagesOnBoarding3 = "assets/images/onBoarding3.jpg";
+}
+
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color primaryColor = const Color(0x00156E97);
+  static Color offwhite = const Color.fromARGB(0, 217, 219, 220);
+  static Color deepColor = const Color.fromARGB(0, 6, 46, 64);
+  static Color grey = const Color.fromARGB(0, 61, 61, 61);
+  static Color black = const Color.fromARGB(0, 0, 0, 0);
+}
